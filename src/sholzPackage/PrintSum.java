45a4a199dbe11472a4f85833383146a329e6ce1c
OginniShola <oginni.shola@yahoo.com>
2015-03-25 11:32:37 +0100

    My first package

---
+++ src/sholzPackage/PrintSum.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sholzPackage;
+
+/**
+ *
+ * @author oginni.olushola
+ */
+public class PrintSum {
+
+//    System.out.print("The answer to 4 + 3 = 7" );
+    //commented on purpose
+    System.out.println("yes");
+}
